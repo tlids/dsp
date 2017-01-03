4c70933d20e0b01e948fcf120204d6f68aa5cc99
xianglf391 <tiffanyli@u.northwestern.edu>
2017-01-03 12:26:29 -0800

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,23 @@
-PLACE YOUR CODE HERE
+# Q1
+
+import pandas as pd
+faculty = pd.read_csv('/Users/xiangli/ds/metis/metisgh/prework/dsp/python/faculty.csv')
+degree = faculty[' degree']
+degree.describe()
+from collections import Counter
+counts = Counter(degree)
+print counts
+
+#Q2
+title = faculty[' title']
+title.describe()
+counts = Counter(title)
+print counts
+
+#Q3
+email = faculty[' email']
+print email
+
+#Q4
+
+
