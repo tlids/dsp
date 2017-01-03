a5bca8b6bf56c335eb80609494b35ad5499f2d53
xianglf391 <tiffanyli@u.northwestern.edu>
2017-01-03 10:26:04 -0800

    Update q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,38 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+# Using the codes below, I found that the team with the smallest difference is Leicester and its difference is -34.
+
+import os
+import pandas as pd
+import numpy as np
+
+cwd = os.getcwd()
+print cwd
+
+os.chdir("/Users/xiangli/ds/metis/metisgh/prework/dsp/python")
+
+football = pd.read_csv('/Users/xiangli/ds/metis/metisgh/prework/dsp/python/football.csv')
+
+football.head()
+
+football.info()
+
+football['diff'] = football['Goals'] - football['Goals Allowed']
+
+minima_r = np.min(football['diff'], axis=0)
+print minima_r
+
+num_row = len(football)
+print num_row
+
+def find_min(data):
+    count = 0
+    for e in data['diff']:
+        if e != minima_r:
+            count = count + 1
+    return count
+  
+  find_min(football)
+  
+  print football['Team'][19]
